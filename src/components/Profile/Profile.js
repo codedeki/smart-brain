08cce34c5bd4fbece90f43b8e6140c594bc96a9c
codedeki <serbian_5@hotmail.com>
2020-05-08 13:49:39 -0400

    Updating Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import'./Profile.css';
 
-const Profile = ({ isProfileOpen, toggleModal, user }) => {
-    return <div className='profile-modal'>
+class Profile extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.props.user.name,
+            age: this.props.user.age,
+            pet: this.props.user.pet
+        }
+    }
+
+    onFormChange = (event) => {
+        switch(event.target.name) {
+            case 'user-name':
+                this.setState({name: event.target.value})
+                break;
+            case 'user-age':
+                this.setState({age: event.target.value})
+                break;
+            case 'user-pet':
+                this.setState({pet: event.target.value})
+                break;
+            default:
+                return;
+        }
+    }
+
+    onProfileUpdate = (data) => {
+        fetch(`http://localhost:3000/profile/${this.props.user.id}`, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ formInput: data })
+        }).then(res => {
+            this.props.toggleModal();
+            this.props.loadUser({ ...this.props.user, ...data });
+        }).catch(console.log)
+    }
+
+    render() {
+        const { user } = this.props;
+        const { name, age, pet } = this.state;
+
+        return (<div className='profile-modal'>
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
         <main className="pa4 black-80 w-80">
             <img 
@@ -10,13 +51,14 @@ const Profile = ({ isProfileOpen, toggleModal, user }) => {
                 className="h3 w3 dib" alt="avatar"
             />
 
-            <h1>{user.name}</h1>
+            <h1>{this.state.name}</h1>
             <h4>{`Image Submitted: ${user.entries}`}</h4>
             <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
             <hr/>
             
             <label className="mt2 fw6" htmlFor="user-name">Name:</label>
             <input
+                onChange={this.onFormChange}
                 className="pa2 ba w-100"
                 placeholder={user.name}
                 type="text"
@@ -25,6 +67,7 @@ const Profile = ({ isProfileOpen, toggleModal, user }) => {
             />
             <label className="mt2 fw6" htmlFor="user-age">Age:</label>
             <input
+                onChange={this.onFormChange}
                 className="pa2 ba w-100"
                 placeholder={user.age}
                 type="text"
@@ -33,6 +76,7 @@ const Profile = ({ isProfileOpen, toggleModal, user }) => {
             />
             <label className="mt2 fw6" htmlFor="user-pet">Pet:</label>
             <input
+                onChange={this.onFormChange}
                 className="pa2 ba w-100"
                 placeholder={user.pet}
                 type="text"
@@ -40,14 +84,17 @@ const Profile = ({ isProfileOpen, toggleModal, user }) => {
                 id="pet"
             />
             <div className="mt4" style={{display: 'flex', justifyContent: 'space-evenly'}}>
-                <button className='b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20'>Save</button>
-                <button className='b pa2 grow pointer hover-white w-40 bg-light-red b--black-20' onClick={toggleModal}>Cancel</button>
-
+                <button onClick={ () => this.onProfileUpdate({ name, age, pet })} className='b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20'>Save</button>
+                <button className='b pa2 grow pointer hover-white w-40 bg-light-red b--black-20' onClick={this.props.toggleModal}>Cancel</button>
             </div>
         </main>
-        <div className='modal-close' onClick={toggleModal}>&times;</div>
+        <div className='modal-close' onClick={this.props.toggleModal}>&times;</div>
       </article>
     </div>
+    );
 }
+
+}
+
 
 export default Profile;
